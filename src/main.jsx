7e1a2dfb13e3389fa72d6e10c5e8953f87bccb41
kayarhabby <pasciahershekaya@gmail.com>
2024-08-06 23:00:59 +0200

    intégration de route

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './assets/components/App.jsx'
 import './assets/css/index.css'
+import router from "./assets/routes/router.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router}/>
   </React.StrictMode>,
 )
