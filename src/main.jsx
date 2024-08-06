52326d8d13cf61f7c54b786f4fe05a5ed848cebf
kayarhabby <pasciahershekaya@gmail.com>
2024-08-06 23:01:36 +0200

    suppression des imports non nécessaires

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './assets/components/App.jsx'
 import './assets/css/index.css'
 import router from "./assets/routes/router.jsx";
 
