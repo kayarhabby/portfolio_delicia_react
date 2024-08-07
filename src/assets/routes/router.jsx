a9c40c13929f98078c1f365a00f5dc8e1c609c5a
kayarhabby <pasciahershekaya@gmail.com>
2024-08-07 15:04:39 +0200

    mise en place du routage

--- src/assets/routes/router.jsx
+++ src/assets/routes/router.jsx
@@ -1,18 +1,11 @@
 import {createBrowserRouter} from "react-router-dom";
 import App from "../components/App.jsx";
-import ArticlePublie from "../components/ArticlePublie.jsx";
 
 const router = createBrowserRouter([
-
     {
         path : '/',
         element : <App />
-    },
-    {
-        path : '/ArticlePublie/:name',
-        element : <ArticlePublie />
     }
-
 ])
 
 export default router;
