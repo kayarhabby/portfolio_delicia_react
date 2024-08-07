851b34a89f488057638b22cb459fd9d7a8c1f129
kayarhabby <pasciahershekaya@gmail.com>
2024-08-07 15:05:56 +0200

    import du module RouterProvider de react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {RouterProvider} from "react-router-dom";
 import './assets/css/index.css'
 import router from "./assets/routes/router.jsx";
 
