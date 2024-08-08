f02f15d9e1b97f0bdb8a08ecb77789a0cac44463
kayarhabby <pasciahershekaya@gmail.com>
2024-08-08 17:31:35 +0200

    remplissage de la page education

--- src/assets/components/Resume.jsx
+++ src/assets/components/Resume.jsx
@@ -3,11 +3,15 @@ import Title from "./Title.jsx";
 
 const Resume = () => {
     return (
-        <div id="resume">
-            <Title title="Resume"/>
-            <section>
-                <h2> Education </h2>
+        <div id="resume" className="resume">
+            <Title title="Education"/>
+            <div className="education_container">
                 <div className="education_content">
+                    <article>
+                        <h3>Études universitaires à la Faculté des Sciences de la Santé</h3>
+                        <h4>2020-2023</h4>
+                        <p>diplôme obtenu : Licence en sciences de la santé option Santé Publique.</p>
+                    </article>
                     <article>
                         <h3>Études universitaires à la Faculté des Sciences de la Santé</h3>
                         <h4>2020-2023</h4>
@@ -20,18 +24,22 @@ const Resume = () => {
                             Brazzaville.
                         </p>
                     </article>
+                </div>
+                <div className="education_content">
                     <article>
-                        <h3>Autres Formation</h3>
-                        <ul>
-                            <li>Certification master de la formation des cadres de niveau intermédiaire (MLM) en Avril 2024</li>
-                            <li>MOOC Agir pour sa santé en Novembre 2023</li>
-                            <li>MOOC Médecine et Santé environnementale en Novembre 2023; </li>
-                            <li>MOOC Rédiger et publier un article scientifique en Octobre 2023, </li>
-                            <li>Badge de réussite du MOOC sur la santé environnementale en Avril 2023 .</li>
-                        </ul>
+                        <h3>Certification master de la formation des cadres de niveau intermédiaire (MLM)</h3>
+                        <h4>2023-2024</h4>
+                    </article>
+                    <article>
+                        <h3>MOOC Rédiger et publier un article scientifique</h3>
+                        <h4> Octobre 2023</h4>
+                    </article>
+                    <article>
+                        <h3>Badge de réussite du MOOC sur la santé environnementale</h3>
+                        <h4>Avril 2023</h4>
                     </article>
                 </div>
-            </section>
+            </div>
 
         </div>
     );
