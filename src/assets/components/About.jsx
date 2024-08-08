2386aa564ff7f49a270e0b1e06b0c422c514d739
kayarhabby <pasciahershekaya@gmail.com>
2024-08-08 15:15:46 +0200

    mise en place de la structure de la page about et remplissage de ce dernier

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import Title from "./Title.jsx";
 
 const About = () => {
     return (
-        <div>
-
-        </div>
+        <article id='about' className='about'>
+            <Title title='About' />
+            <section>
+                <img src="" alt=""/>
+                <article>
+                    <p>Je suis KISSANGOU-OKAKA Délicia-Raelle, une étudiante passionnée de santé publique à l'Université
+                        Marien NGOUABI au Congo. Membre actif de la communauté Épanouis (EPAN8) ainsi que U-Reporter by
+                        Unicef, je m'engage dans la promotion de la santé et la prévention des maladies. En dehors de
+                        mes études, je trouve la détente dans la lecture, les jeux vidéo et la musique. J'adore assister
+                        à des conférences pour élargir mes connaissances et me connecter avec des professionnels
+                        partageant les mêmes intérêts. Mon objectif est de devenir une professionnelle qualifiée
+                        et influente dans le domaine de la santé publique, afin d'influencer les politiques et pratiques
+                        de santé à l'échelle mondiale.</p>
+                    <button>
+                        <img src="" alt=""/>
+                        Dowload CV
+                    </button>
+                </article>
+            </section>
+        </article>
     );
 };
 
