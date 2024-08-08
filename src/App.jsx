3c9cedd1f3ecb74172115a2d4955d009c22b6e93
kayarhabby <pasciahershekaya@gmail.com>
2024-08-08 15:25:36 +0200

    ajout component About, Resume et Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './assets/css/App.css'
 import Navbar from "./assets/components/Navbar.jsx";
+import Header from "./assets/components/Header.jsx";
+import About from "./assets/components/About.jsx";
+import Resume from "./assets/components/Resume.jsx";
 
 function App() {
 
   return (
     <div>
       <Navbar />
+      <Header />
+      <About />
+      <Resume />
     </div>
   )
 }
