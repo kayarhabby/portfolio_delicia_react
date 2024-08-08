8f5e5c6ad9fd85876b89a5f7d3ea596d3739ce33
kayarhabby <pasciahershekaya@gmail.com>
2024-08-08 15:50:11 +0200

    ajout image dans le header

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function Header() {
     return (
         <header className="header__container">
+            <img src="/src/assets/images/kimLee.jpg" alt="kim lee" className="cercle_img"/>
             <article className="header__article_content">
                 <p>Bonjour, je suis KISSANGOU OKAKA.</p>
                 <p>Etudiante passionnée de santé publique à l'Université Marien NGOUABI au Congo.</p>
