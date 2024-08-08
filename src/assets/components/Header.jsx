2336035595aa814bf202dae7ad39492857caba24
kayarhabby <pasciahershekaya@gmail.com>
2024-08-08 15:17:54 +0200

    mise en place de la structure de la page de présentation

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header__container">
+            <article className="header__article_content">
+                <p>Bonjour, je suis KISSANGOU OKAKA.</p>
+                <p>Etudiante passionnée de santé publique à l'Université Marien NGOUABI au Congo.</p>
+            </article>
+            <button className="btn">Let's Get Started</button>
+        </header>
+    );
+}
