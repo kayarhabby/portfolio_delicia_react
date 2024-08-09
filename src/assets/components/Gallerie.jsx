29b4ad6725b67c157b15342e502697d76657f867
kayarhabby <pasciahershekaya@gmail.com>
2024-08-09 20:54:17 +0200

    terminaison de la page gallerie

--- src/assets/components/Gallerie.jsx
+++ src/assets/components/Gallerie.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import Title from "./Title.jsx";
 
 const Gallerie = () => {
+    const images = [
+        { src: "/src/assets/images/techno.jpg", alt: "techno", title: "Conferences" },
+        { src: "/src/assets/images/nature_fleur.jpg", alt: "nature_fleur", title: "Conferences" },
+        { src: "/src/assets/images/nature_fleur.jpg", alt: "nature_fleur", title: "Conferences" },
+        { src: "/src/assets/images/nature_fleur.jpg", alt: "nature_fleur", title: "Conferences" }
+    ];
     return (
-        <div>
-            
+        <div id='gallerie' className='gallerie'>
+            <Title title="Gallerie" />
+            <section className='gallerie_container'>
+                {images.map((image, index) => (
+                    <article className="gallerie_content" key={index}>
+                        <div className="gallerie_items_img">
+                            <img src={image.src} alt={image.alt} />
+                            <h4>{image.title}</h4>
+                        </div>
+                    </article>
+                ))}
+            </section>
         </div>
     );
 };
