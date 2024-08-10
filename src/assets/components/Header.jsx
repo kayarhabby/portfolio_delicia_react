46d300df5041b7d60aac1f69233d9413cf865d6c
kayarhabby <pasciahershekaya@gmail.com>
2024-08-10 13:02:54 +0200

    traduction du composant Header

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import "../css/Header.css";
 
 export default function Header() {
+    const { t } = useTranslation('header');
+
     return (
         <header className="header__container">
             <img src="/src/assets/images/kimLee.jpg" alt="kim lee" className="cercle_img"/>
             <article className="header__article_content">
-                <p>Bonjour, je suis KISSANGOU OKAKA.</p>
-                <p>Etudiante passionnée de santé publique à l'Université Marien NGOUABI au Congo.</p>
+                <p>{t('header.greeting')}</p>
+                <p>{t('header.description')}</p>
             </article>
-            <button className="btn">Let's Get Started</button>
+            <button className="btn">{t('header.cta')}</button>
         </header>
     );
 }
