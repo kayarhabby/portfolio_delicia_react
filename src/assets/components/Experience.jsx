1fe99527020c45c5ea39794eef85baa20200592f
kayarhabby <pasciahershekaya@gmail.com>
2024-08-11 00:09:45 +0200

    remodification de la structure

--- src/assets/components/Experience.jsx
+++ src/assets/components/Experience.jsx
@@ -11,40 +11,42 @@ const Experience = () => {
     const professionalExperience = t('experience.professionalExperience', { returnObjects: true });
 
     return (
-        <div id="experience" className="experience">
-            <Title title={t('experience.title')} />
-            <section className="experience_container">
-                <div>
-                    <section className="experience_icon_title">
-                        <i className="fa-solid fa-business-time fa-2x"></i>
-                        <h2>{t('experience.subtitle')}</h2>
-                    </section>
-                    <section className="experience_content">
-                        {stages.map((exp, index) => (
-                            <article className="experience_items" key={index}>
-                                <h3>{exp.title}</h3>
-                                <h4>{exp.period}</h4>
-                                <small>{exp.description}</small>
-                            </article>
-                        ))}
-                    </section>
-                </div>
+        <div id="experience" >
+            <section className="experience">
+                <Title title={t('experience.title')} />
+                <section className="experience_container">
+                    <div>
+                        <section className="experience_icon_title">
+                            <i className="fa-solid fa-business-time fa-2x"></i>
+                            <h2>{t('experience.subtitle')}</h2>
+                        </section>
+                        <section className="experience_content">
+                            {stages.map((exp, index) => (
+                                <article className="experience_items" key={index}>
+                                    <h3>{exp.title}</h3>
+                                    <h4>{exp.period}</h4>
+                                    <small>{exp.description}</small>
+                                </article>
+                            ))}
+                        </section>
+                    </div>
 
-                <div>
-                    <section className="experience_icon_title">
-                        <i className="fa-solid fa-briefcase fa-2x"></i>
-                        <h2>{t('experience.subtitle2')}</h2>
-                    </section>
-                    <section className="experience_content">
-                        {professionalExperience.map((exp, index) => (
-                            <article className="experience_items" key={index}>
-                                <h3>{exp.title}</h3>
-                                <h4>{exp.period}</h4>
-                                <small>{exp.description}</small>
-                            </article>
-                        ))}
-                    </section>
-                </div>
+                    <div>
+                        <section className="experience_icon_title">
+                            <i className="fa-solid fa-briefcase fa-2x"></i>
+                            <h2>{t('experience.subtitle2')}</h2>
+                        </section>
+                        <section className="experience_content">
+                            {professionalExperience.map((exp, index) => (
+                                <article className="experience_items" key={index}>
+                                    <h3>{exp.title}</h3>
+                                    <h4>{exp.period}</h4>
+                                    <small>{exp.description}</small>
+                                </article>
+                            ))}
+                        </section>
+                    </div>
+                </section>
             </section>
         </div>
     );
