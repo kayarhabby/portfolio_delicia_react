ce8eba01e88cacad49a26560fe749905787b5ef1
kayarhabby <pasciahershekaya@gmail.com>
2024-08-10 13:01:45 +0200

    traduction du composant Education

--- src/assets/components/Education.jsx
+++ src/assets/components/Education.jsx
@@ -1,48 +1,13 @@
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import Title from "./Title.jsx";
 import "../css/Education.css";
 
 const Education = () => {
+    const { t } = useTranslation('education');
 
-    // Exemple de données d'éducation, remplace avec tes propres données si nécessaire
-    const educationData = [
-        {
-            imgSrc: "/src/assets/images/nature_fleur.jpg",
-            date: "jan 12, 2024",
-            title: "Licence en sciences de la santé option Santé Publique",
-            description: "Études universitaires à la Faculté des Sciences de la Santé, diplôme obtenu : Licence en sciences de la santé option Santé Publique."
-        },
-        {
-            imgSrc: "/src/assets/images/techno.jpg",
-            date: "jan 12, 2024",
-            title: "Baccalauréat Scientifique",
-            description: "Secondaire deuxième cycle, diplôme obtenu : Baccalauréat série C, mention assez-bien au lycée de la réconciliation, Brazzaville."
-        },
-        {
-            imgSrc: "/src/assets/images/techno.jpg",
-            date: "Avril 2024",
-            title: "MLM",
-            description: "Obtention d'une certification master de la formation des cadres de niveau intermédiaire (MLM)."
-        },
-        {
-            imgSrc: "/src/assets/images/techno.jpg",
-            date: "Novembre 2023",
-            title: "MOOC",
-            description: "Agir pour sa santé."
-        },
-        {
-            imgSrc: "/src/assets/images/techno.jpg",
-            date: "Octobre 2023",
-            title: "MOOC",
-            description: "Rédiger et publier un article scientifique."
-        },
-        {
-            imgSrc: "/src/assets/images/techno.jpg",
-            date: "Avril 2023",
-            title: "MOOC",
-            description: "Badge de réussite du MOOC sur la santé environnementale."
-        }
-    ];
+    // Obtention des données d'éducation traduites
+    const educationData = t('education.items', { returnObjects: true });
 
     // État pour gérer le nombre d'articles affichés
     const [visibleCount, setVisibleCount] = useState(3);
@@ -54,7 +19,7 @@ const Education = () => {
 
     return (
         <div id='education' className='education'>
-            <Title title='Education' />
+            <Title title={t('education.title')} />
             <section className="education_container">
                 {educationData.slice(0, visibleCount).map((item, index) => (
                     <article className="education_content" key={index}>
@@ -69,8 +34,8 @@ const Education = () => {
                             <h3>{item.title}</h3>
                             <small>{item.description}</small>
                             <div className="readmoreContainer">
-                                <a href="">CERTIFICAT</a>
-                                <a href=""><i className="fa-solid fa-arrow-right"></i></a>
+                                <a href="#">CERTIFICAT</a>
+                                <a href="#"><i className="fa-solid fa-arrow-right"></i></a>
                             </div>
                         </div>
                     </article>
