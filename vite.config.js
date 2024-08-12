f83b635689ef3ae10ad97465bfb726cf0006450a
kayarhabby <pasciahershekaya@gmail.com>
2024-08-12 15:40:28 +0200

    fix : deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/portfolio_delicia_react/",
   plugins: [react()],
-  base:"/portfolio_delicia_react/"
 })
